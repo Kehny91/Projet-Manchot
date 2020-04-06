22c775c560ff680f9728bbc1d36436b203c6e8e1
tomjtt <56792173+tomjtt@users.noreply.github.com>
2020-04-06 19:15:24 +0200

    Update Espace.py

--- Espace.py
+++ Espace.py
@@ -15,13 +15,14 @@ def moduloF(x, modulo):
         return x - (x//modulo)*modulo
     else:
         return x - (x//modulo+1)*modulo
+        
 
 """renvoie un angle quelconque dans l'intervalle -pi pi
     @param float : angle en radian
     @throws AngleException : l'angle n'existe pas
 """   
-def normalise(angle):  
-    angle = moduloF(angle,2*np.pi)
+def normalise(angle):
+    angle = moduloF(angle, 2*np.pi)
     if -np.pi<angle and angle<=np.pi:
         return angle
     elif angle<=-np.pi:
@@ -31,16 +32,32 @@ def normalise(angle):
     else:
         assert False,"angle not real: " + str(angle)
 
+"""classe point et ses methodes
+    attriute float : x, coordonnee x
+    attriute float : z, coordonnee z
+"""
+class Point:
+    
+    def __init__(self,x,z):
+        self.x=x
+        self.z=z
+        
+    def distance(self,pointB):
+        return ((self.x-pointB.x)**2+(self.z-pointB.z)**2)**0.5
+        
+    def __str__(self):
+        return("les coordonnees du point sont :"+'('+ str(self.x)+','+str(self.z)+')')
+        
 """classe Referentiel et ses methodes
     attribute String : nom, nom du referentiel
     attribute float : angleAxeY, angle par rapport a l'horizontale
 """
-
 class Referentiel:
     
-    def __init__(self,nom,angleAxeY):
+    def __init__(self,nom,angleAxeY, origine):
         self.nom=nom
         self.angleAxeY= angleAxeY
+        self.origine = Point(0,0)
     
     def getNom(self):
         return self.nom
@@ -53,13 +70,25 @@ class Referentiel:
     
     def setAngleAxeY(self, newAngleAxeY):
         self.angleAxeY = newAngleAxeY
+        
+    def getOrigine(self):
+        return self.origine
     
-    def __affiche__(self):
-        print("le referenciel a pour nom : " + str(self.nom) + " et pour angle par rapport à l'horizontale : " + str(self.angleAxeY))
+    def setOrigine(self,newOrigine):
+        self.origine = newOrigine
+        
+    def __eq__(self, ref):
+        if (self.nom == ref.nom and self.angleAxeY == ref.angleAxeY):
+            return True
+        else:
+            return False
+            
+    def __str__(self):
+        return "le referenciel a pour nom : " + str(self.nom) + ", pour origine : " + str(self.origine)  + " et pour angle par rapport à l'horizontale : " + str(self.angleAxeY)
 
 """ classe Vecteur et ses methodes
-    attribute float : x, coordonnee x
-    attribute float : z, coordonnee z
+    attribute float : x, composante x
+    attribute float : z, composante z
     attribute Referentiel : ref, referentiel utilisé
 """     
 class Vecteur:
@@ -68,7 +97,7 @@ class Vecteur:
         self.z=z
         self.ref = ref
         
-    def __changeRef__(self,ref):
+    def changeRef(self,ref):
         angleRefY = normalise(self.ref.angleAxeY-ref.angleAxeY)
         x = np.cos(angleRefY)*self.x - np.sin(angleRefY)*self.z
         z = np.cos(angleRefY)*self.z + np.sin(angleRefY)*self.x
@@ -78,34 +107,35 @@ class Vecteur:
         if self.ref == vecteur.ref:
             return Vecteur(self.x+vecteur.x, self.z+vecteur.z, self.ref)
         else : 
-            return self.__add__(vecteur.__changeRef__(self.ref))
+            return self.__add__(vecteur.changeRef(self.ref))
             
-    def __sous__(self,vecteur):
+    def __sub__(self,vecteur):
         if self.ref == vecteur.ref:
             return Vecteur(self.x-vecteur.x, self.z-vecteur.z, self.ref)
         else : 
-            return self.__sous__(vecteur.__changeRef__(self.ref))
+            return self.__sub__(vecteur.changeRef(self.ref))
             
-    def __prodScal__(self, vecteur):
+    def prodScal(self, vecteur):
         if self.ref == vecteur.ref:
             return self.x*vecteur.z - self.z*vecteur.x
         else:
-            return self.__prodScal__(vecteur.__changeRef__(self.ref))
+            return self.prodScal(vecteur.changeRef(self.ref))
     
-    def __norm__(self):
+    def norm(self):
         return np.sqrt(self.x**2+self.z**2)
         
-    def __unitaire__(self):
+    def unitaire(self):
         if self.x==0 and self.y==0:
             return Vecteur(1,0)
         else :
             n= self.norm()
             return Vecteur(self.x/n,self.z/n)
             
-    def __affiche__(self):
-        print("Dans le ref : " + str(self.ref.nom) + " les coordonnees sont (" + str(self.x) +"," + str(self.z) + ")")
+    def __str__(self):
+        return "Dans le ref : " + str(self.ref.nom) + " les coordonnees sont (" + str(self.x) +"," + str(self.z) + ")"
     
-    def __afficheNorm__(self):
-        print("Dans le ref : " + str(self.ref.nom) + "la norme est : " + str(self.__norm__()))
-        
-    
+    def afficheNorm(self,ref):
+        if self.ref==ref:
+            print "Dans le ref : " + str(self.ref.nom) + "la norme est : " + str(self.norm())
+        else:
+            return self.changeRef(ref).afficheNorm(ref)
